e5b70ac0324835c6e3174103c5c02adbe715552c
Limax <limax.workspace@gmail.com>
2024-03-29 23:12:39 +0100

    Added Schema for user

--- convex/schema.ts
+++ convex/schema.ts
@@ -7,4 +7,47 @@ export default defineSchema({
     text: v.string(),
     
   }),
+
+  users: defineTable({
+    name: v.string(),
+    email: v.string(),
+    image: v.string(),
+    id: v.id("users"),
+    calendarEntry: v.object({
+      title: v.string(),
+      description: v.string(),
+      date: v.string(),
+      type: v.string(),
+      id: v.id("calendarEntry"),
+    }),
+    note: v.object({
+      title: v.string(),
+      description: v.string(),
+      subject: v.optional(v.string()),
+      dueDate: v.optional(v.string()),
+      showInCalender: v.boolean(),
+      id: v.id("note"),
+    }),
+    task: v.object({
+      title: v.string(),
+      description: v.string(),
+      subject: v.optional(v.string()),
+      dueDate: v.optional(v.string()),
+      showInCalender: v.boolean(),
+      id: v.id("task"),
+    }),
+    provider: v.optional(v.string()),
+    grade: v.object({
+      subject: v.object({
+        grade: v.object({
+          date: v.string(),
+          topic: v.string(),
+          type: v.string(),
+          id: v.id("grade"),
+        }),
+        subjectAverage: v.string()
+      }),
+      totalAverage: v.string(),
+    })
+  }),
 });
