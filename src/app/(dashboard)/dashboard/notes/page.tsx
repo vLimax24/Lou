a16b2d5e2a3828dba6b63916176d9a546f2527da
Limax <limax.workspace@gmail.com>
2024-04-03 20:31:52 +0200

    added notes section

--- src/app/(dashboard)/dashboard/notes/page.tsx
+++ src/app/(dashboard)/dashboard/notes/page.tsx
@@ -1,9 +1,123 @@
-import React from 'react'
+'use client'
 
-const page = () => {
+import React from 'react'
+import { AddNoteDialog } from './addNoteDialog'
+import { Trash, CalendarDays, Pencil } from 'lucide-react'
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/components/ui/tooltip"
+import { Button } from "@/components/ui/button"
+
+const notes = [
+  {
+    text: 'this is a test note',
+    dueDate: '2024-10-12',
+    showInCalendar: true,
+  },
+  {
+    text: 'this is a test note',
+    dueDate: '2024-10-12',
+    showInCalendar: true,
+  },
+  {
+    text: 'this is a test note',
+    dueDate: '2024-10-12',
+    showInCalendar: true,
+  },
+  {
+    text: 'this is a test note',
+    dueDate: '2024-10-12',
+    showInCalendar: true,
+  },
+  {
+    text: 'this is a test note',
+    dueDate: '2024-10-12',
+    showInCalendar: true,
+  },
+  {
+    text: 'this is a test note',
+    dueDate: '2024-10-12',
+    showInCalendar: true,
+  },
+  {
+    text: 'this is a test note',
+    dueDate: '2024-10-12',
+    showInCalendar: true,
+  },
+  {
+    text: 'this is a test note',
+    dueDate: '2024-10-12',
+    showInCalendar: true,
+  },
+  {
+    text: 'this is a test note',
+    dueDate: '2024-10-12',
+    showInCalendar: true,
+  },
+  {
+    text: 'this is a test note',
+    dueDate: '2024-10-12',
+    showInCalendar: true,
+  },
+]
+
+const Page = () => {
   return (
-    <div>page</div>
+    <div className='p-5'>
+      <div className="flex items-center justify-between mb-6">
+        <h1 className='text-4xl font-bold'>Your Pinboard</h1>
+        <AddNoteDialog />
+      </div>
+      <div className='grid grid-cols-5 gap-4'>
+        {notes.map((note) => (
+          <div
+            key={note.text}
+            className="border border-gray-200 rounded-md p-4 bg-white shadow-md flex-grow h-32 flex flex-col justify-between"
+          >
+            <div className="flex flex-col items-start justify-start">
+              <p className="text-lg font-semibold">{note.text}</p>
+              
+            </div>
+            <div className='mt-auto text-gray-700 flex w-full items-center justify-between'>
+            <p className="text-sm text-gray-500">{note.dueDate}</p>
+            <TooltipProvider>
+              <div className='flex items-center'>
+                <div>
+                  <Tooltip delayDuration={50}>
+                    <TooltipTrigger asChild>
+                        <CalendarDays size={20} className='hover:cursor-pointer mx-1 hover:text-green-500 duration-300' />
+                    </TooltipTrigger>
+                    <TooltipContent>
+                      <p>View in Calendar</p>
+                    </TooltipContent>
+                  </Tooltip>
+                </div>
+                <div>
+                  <Tooltip delayDuration={50}>
+                    <TooltipTrigger asChild>
+                        <Trash size={20} className='hover:cursor-pointer mx-1 hover:text-green-500 duration-300' />
+                    </TooltipTrigger>
+                    <TooltipContent>
+                      <p>Delete Note</p>
+                    </TooltipContent>
+                  </Tooltip>
+                </div>
+                <div>
+                  <Tooltip delayDuration={50}>
+                    <TooltipTrigger asChild>
+                        <Pencil size={20} className='hover:cursor-pointer mx-1 hover:text-green-500 duration-300' />
+                    </TooltipTrigger>
+                    <TooltipContent>
+                      <p>Edit Note</p>
+                    </TooltipContent>
+                  </Tooltip>
+                </div>
+              </div>
+            </TooltipProvider>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
   )
 }
 
-export default page
+export default Page
--- src/app/(dashboard)/dashboard/tasks/page.tsx
+++ src/app/(dashboard)/dashboard/tasks/page.tsx
@@ -12,25 +12,29 @@ import { Draggable } from './Draggable';
 type TaskStatus = 'PENDING' | 'IN-PROGRESS' | 'COMPLETED';
 
 interface Task {
-  _id: unknown;
+  _id: string; // Assuming _id is a string
   text: string;
   status: TaskStatus;
 }
 
-const TaskTypes: Record<string, TaskStatus> = {
+const taskTypes: Record<string, TaskStatus> = {
   todo: 'PENDING',
   inProgress: 'IN-PROGRESS',
   completed: 'COMPLETED',
 };
 
+const capitalizeFirstLetter = (str: string): string => {
+  return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+};
+
 const Tasks = (): JSX.Element => {
   const { isAuthenticated } = useConvexAuth();
-  const tasks = useQuery(
+  const tasks = useQuery<Task[]>(
     api.tasks.getTasks,
     !isAuthenticated ? 'skip' : undefined
   );
-  const updateTask = useMutation(api.tasks.updateTaskStatus);
-  const containers: TaskStatus[] = Object.values(TaskTypes);
+  const updateTask = useMutation<{ taskId: string, newStatus: TaskStatus }>(api.tasks.updateTaskStatus);
+  const containers: TaskStatus[] = Object.values(taskTypes);
   const [parent, setParent] = useState<string | null>(null);
 
   async function handleDragEnd(event: DragEndEvent): Promise<void> {
@@ -40,7 +44,7 @@ const Tasks = (): JSX.Element => {
     const taskId: string = active.id as string;
 
     // Update task status
-    await updateTask({ taskId, newStatus });
+    await updateTask.mutateAsync({ taskId, newStatus });
     setParent(null); // Reset parent
   }
 
@@ -50,7 +54,7 @@ const Tasks = (): JSX.Element => {
       <div className='flex w-full justify-between'>
         {containers.map((type: TaskStatus) => (
           <div key={type} className="w-1/3 p-4">
-            <h2 className="text-lg font-bold mb-4">{type}</h2>
+            <h2 className="text-lg font-bold mb-4">{capitalizeFirstLetter(type.replace('-', ' '))}</h2>
             <Droppable key={type} id={type}>
               {tasks
                 ?.filter((task) => task.status === type)
