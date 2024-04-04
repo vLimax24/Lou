13c6d85c3ee09611ea571f4f8d1edb49eb493658
Limax <limax.workspace@gmail.com>
2024-04-04 21:31:27 +0200

    updated typescript errors

--- src/app/(dashboard)/dashboard/tasks/Draggable.tsx
+++ src/app/(dashboard)/dashboard/tasks/Draggable.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
-import {useDraggable} from '@dnd-kit/core';
+import { useDraggable, UseDraggableArguments } from '@dnd-kit/core';
 
-export function Draggable(props) {
-  const {attributes, listeners, setNodeRef, transform} = useDraggable({
+interface DraggableProps {
+  id: string;
+  children: React.ReactNode;
+}
+
+export function Draggable(props: DraggableProps) {
+  const { attributes, listeners, setNodeRef, transform } = useDraggable({
     id: props.id,
-  });
+  } as UseDraggableArguments);
   const style = transform ? {
     transform: `translate3d(${transform.x}px, ${transform.y}px, 0)`,
   } : undefined;
 
-  
   return (
     <button ref={setNodeRef} style={style} {...listeners} {...attributes} className='w-full'>
       {props.children}
--- src/app/(dashboard)/dashboard/tasks/Droppable.tsx
+++ src/app/(dashboard)/dashboard/tasks/Droppable.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
-import {useDroppable} from '@dnd-kit/core';
+import { useDroppable, UseDroppableArguments } from '@dnd-kit/core';
 
-export function Droppable(props) {
-  const {isOver, setNodeRef} = useDroppable({
+interface DroppableProps {
+  id: string;
+  children: React.ReactNode;
+}
+
+export function Droppable(props: DroppableProps) {
+  const { isOver, setNodeRef } = useDroppable({
     id: props.id,
-  });
+  } as UseDroppableArguments);
   const style = {
     color: isOver ? 'green' : undefined,
   };
-  
-  
+
   return (
     <div ref={setNodeRef} style={style} className='w-full min-h-20 border border-gray-500 rounded-lg p-2 flex-grow mr-2 last:mr-0'>
       {props.children}
