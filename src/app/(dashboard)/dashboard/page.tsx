f751ad297dd9be97bb15abdc10dea91e0cf01f48
Limax <limax.workspace@gmail.com>
2024-05-24 10:51:43 +0200

    update notification

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -2,27 +2,24 @@
 import AssignmentCard from "@/components/dashboard/home/Assignments"
 import NotesCard from "@/components/dashboard/home/Notes"
 import { GradeBarChart } from "@/components/dashboard/home/GradeSheet/BarChart"
-import ActionBar from "@/components/dashboard/home/Search"
+// import ActionBar from "@/components/dashboard/home/Search"
 import PomodoroTimerCard from "@/components/dashboard/home/PomodoroTimer"
 
 const Dashboard = () => {
-  
   return (
-    
-      <div className="flex flex-col rounded-lg justify-center">
-        <div className='flex flex-col md:flex-row flex-1 pb-1'>
-          <div className="center flex-col">
-            <ActionBar />
-            <GradeBarChart />
-          </div>
-          <PomodoroTimerCard />
+    <div className="flex flex-col justify-center rounded-lg">
+      <div className="flex flex-1 flex-col pb-1 md:flex-row">
+        <div className="center flex-col">
+          {/* <ActionBar /> */}
+          <GradeBarChart />
         </div>
-        <div className='flex flex-1 pt-1 pb-0 flex-col md:flex-row justify-center'>
-          <AssignmentCard />
-          <NotesCard />
-        </div>
+        <PomodoroTimerCard />
       </div>
-
+      <div className="flex flex-1 flex-col justify-center pb-0 pt-1 md:flex-row">
+        <AssignmentCard />
+        <NotesCard />
+      </div>
+    </div>
   )
 }
 
