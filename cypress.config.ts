13ab96fdc12d053ee6adf4b6cd70fa7407f343ee
Limax <limax.workspace@gmail.com>
2024-06-03 16:35:02 +0200

    add CI

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,11 +1,18 @@
-import { defineConfig } from "cypress"
+import { defineConfig } from "cypress";
 
 export default defineConfig({
   projectId: "owgfbr",
+
   component: {
     devServer: {
       framework: "next",
       bundler: "webpack",
     },
   },
-})
+
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
---
+++ cypress/e2e/signup_spec.cy.ts
@@ -0,0 +1,6 @@
+/// <reference types="cypress" />
+describe("Signup Auth FLow", () => {
+  it("passes", () => {
+    cy.visit("localhost:3000")
+  })
+})
