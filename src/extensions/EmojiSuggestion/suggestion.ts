71a45ae45a5cd30cd529a489936a27507db7a6e1
Limax <limax.workspace@gmail.com>
2024-06-10 13:05:30 +0200

    update emoji suggestion

--- src/extensions/EmojiSuggestion/suggestion.ts
+++ src/extensions/EmojiSuggestion/suggestion.ts
@@ -1,25 +1,26 @@
-import { ReactRenderer } from '@tiptap/react'
-import { Editor } from '@tiptap/core'
-import { SuggestionKeyDownProps, SuggestionProps } from '@tiptap/suggestion'
-import tippy, { Instance } from 'tippy.js'
+import { ReactRenderer } from "@tiptap/react"
+import { Editor } from "@tiptap/core"
+import { SuggestionKeyDownProps, SuggestionProps } from "@tiptap/suggestion"
+import tippy, { Instance } from "tippy.js"
 
-import EmojiList from './components/EmojiList'
+import EmojiList from "./components/EmojiList"
 
 export const emojiSuggestion = {
   items: ({ editor, query }: { editor: Editor; query: string }) =>
     editor.storage.emoji.emojis
       .filter(
         ({ shortcodes, tags }: { shortcodes: string[]; tags: string[] }) =>
-          shortcodes.find(shortcode => shortcode.startsWith(query.toLowerCase())) ||
-          tags.find(tag => tag.startsWith(query.toLowerCase())),
+          shortcodes.find(shortcode =>
+            shortcode.startsWith(query.toLowerCase())
+          ) || tags.find(tag => tag.startsWith(query.toLowerCase()))
       )
       .slice(0, 250),
 
   allowSpaces: false,
 
   render: () => {
-    let component: ReactRenderer
-    let popup: Instance
+    let component: any
+    let popup: any
 
     return {
       onStart: (props: SuggestionProps<any>) => {
@@ -28,14 +29,16 @@ export const emojiSuggestion = {
           editor: props.editor,
         })
 
-        popup = tippy('body', {
-          getReferenceClientRect: props.clientRect,
+        popup = tippy(document.body, {
+          getReferenceClientRect: props.clientRect as () =>
+            | DOMRect
+            | ClientRect,
           appendTo: () => document.body,
           content: component.element,
           showOnCreate: true,
           interactive: true,
-          trigger: 'manual',
-          placement: 'bottom-start',
+          trigger: "manual",
+          placement: "bottom-start",
         })
       },
 
@@ -43,12 +46,14 @@ export const emojiSuggestion = {
         component.updateProps(props)
 
         popup[0].setProps({
-          getReferenceClientRect: props.clientRect,
+          getReferenceClientRect: props.clientRect as () =>
+            | DOMRect
+            | ClientRect,
         })
       },
 
       onKeyDown(props: SuggestionKeyDownProps) {
-        if (props.event.key === 'Escape') {
+        if (props.event.key === "Escape") {
           popup[0].hide()
           component.destroy()
 
