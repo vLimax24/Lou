6a25d3077ababdf8f21aba50ed98847dc8788362
Limax <limax.workspace@gmail.com>
2024-06-10 12:43:24 +0200

    update type error

--- src/components/menus/ContentItemMenu/hooks/useContentItemActions.tsx
+++ src/components/menus/ContentItemMenu/hooks/useContentItemActions.tsx
@@ -1,15 +1,19 @@
-import { Node } from '@tiptap/pm/model'
-import { NodeSelection } from '@tiptap/pm/state'
-import { Editor } from '@tiptap/react'
-import { useCallback } from 'react'
+import { Node } from "@tiptap/pm/model"
+import { NodeSelection } from "@tiptap/pm/state"
+import { Editor } from "@tiptap/react"
+import { useCallback } from "react"
 
-const useContentItemActions = (editor: Editor, currentNode: Node | null, currentNodePos: number) => {
+const useContentItemActions = (
+  editor: Editor,
+  currentNode: Node | null,
+  currentNodePos: number
+) => {
   const resetTextFormatting = useCallback(() => {
     const chain = editor.chain()
 
     chain.setNodeSelection(currentNodePos).unsetAllMarks()
 
-    if (currentNode?.type.name !== 'paragraph') {
+    if (currentNode?.type.name !== "paragraph") {
       chain.setParagraph()
     }
 
@@ -20,43 +24,68 @@ const useContentItemActions = (editor: Editor, currentNode: Node | null, current
     editor.commands.setNodeSelection(currentNodePos)
 
     const { $anchor } = editor.state.selection
-    const selectedNode = $anchor.node(1) || (editor.state.selection as NodeSelection).node
+    const selectedNode =
+      $anchor.node(1) || (editor.state.selection as NodeSelection).node
 
     editor
       .chain()
-      .setMeta('hideDragHandle', true)
-      .insertContentAt(currentNodePos + (currentNode?.nodeSize || 0), selectedNode.toJSON())
+      .setMeta("hideDragHandle", true)
+      .insertContentAt(
+        currentNodePos + (currentNode?.nodeSize || 0),
+        selectedNode.toJSON()
+      )
       .run()
   }, [editor, currentNodePos, currentNode?.nodeSize])
 
   const copyNodeToClipboard = useCallback(() => {
-    editor.chain().setMeta('hideDragHandle', true).setNodeSelection(currentNodePos).run()
+    editor
+      .chain()
+      .setMeta("hideDragHandle", true)
+      .setNodeSelection(currentNodePos)
+      .run()
 
-    document.execCommand('copy')
+    document.execCommand("copy")
   }, [editor, currentNodePos])
 
   const deleteNode = useCallback(() => {
-    editor.chain().setMeta('hideDragHandle', true).setNodeSelection(currentNodePos).deleteSelection().run()
+    editor
+      .chain()
+      .setMeta("hideDragHandle", true)
+      .setNodeSelection(currentNodePos)
+      .deleteSelection()
+      .run()
   }, [editor, currentNodePos])
 
   const handleAdd = useCallback(() => {
     if (currentNodePos !== -1) {
       const currentNodeSize = currentNode?.nodeSize || 0
       const insertPos = currentNodePos + currentNodeSize
-      const currentNodeIsEmptyParagraph = currentNode?.type.name === 'paragraph' && currentNode?.content?.size === 0
-      const focusPos = currentNodeIsEmptyParagraph ? currentNodePos + 2 : insertPos + 2
+      const currentNodeIsEmptyParagraph =
+        currentNode?.type.name === "paragraph" &&
+        currentNode?.content?.size === 0
+      const focusPos = currentNodeIsEmptyParagraph
+        ? currentNodePos + 2
+        : insertPos + 2
 
       editor
         .chain()
         .command(({ dispatch, tr, state }) => {
           if (dispatch) {
-            if (currentNodeIsEmptyParagraph) {
-              tr.insertText('/', currentNodePos, currentNodePos + 1)
+            const paragraphNode = state.schema.nodes.paragraph
+            if (paragraphNode) {
+              if (currentNodeIsEmptyParagraph) {
+                tr.insertText("/", currentNodePos, currentNodePos + 1)
+              } else {
+                tr.insert(
+                  insertPos,
+                  paragraphNode.create(null, [state.schema.text("/")])
+                )
+              }
+              return dispatch(tr)
             } else {
-              tr.insert(insertPos, state.schema.nodes.paragraph.create(null, [state.schema.text('/')]))
+              console.error("Paragraph node type is not defined in the schema.")
+              return false
             }
-
-            return dispatch(tr)
           }
 
           return true
